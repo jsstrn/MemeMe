3ed429c05054f27a87fa208a29c571f2fa982ce4
jsstrn <jessternrays@gmail.com>
2015-05-18 18:12:13 +0800

    Added an image picker

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
-
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate,  UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: Initial Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    // MARK: Image Picker
+    
+    @IBAction func pickImageFromPhotoLibrary(sender: UIBarButtonItem) {
+        imagePicker(.PhotoLibrary)
+    }
+    
+    func imagePicker(sourceType: UIImagePickerControllerSourceType) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        imagePickerController.delegate = self
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Memory Warning
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
