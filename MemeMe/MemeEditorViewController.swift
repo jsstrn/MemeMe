de2eab0b3e354bf29e0b470128d688ee1daa856f
jsstrn <jessternrays@gmail.com>
2015-05-18 17:20:55 +0800

    Initial commit

---
+++ MemeMe/MemeEditorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MemeEditorViewController.swift
+//  MemeMe
+//
+//  Created by Jesstern Rays on 18/5/15.
+//  Copyright (c) 2015 Jesstern Rays. All rights reserved.
+//
+
+import UIKit
+
+class MemeEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
